f98cd9f32edee5dba2e052dca4ad55584a482307
DESKTOP-3GKT9EU\matha <mathavan18@gmail.com>
2020-12-11 15:43:52 -0500

    Added buttons and its functionalities

--- exercises/beatBoxJS/utils.js
+++ exercises/beatBoxJS/utils.js
@@ -1,35 +1,56 @@
 /**
  * Beat class that keeps track of playing the audio
  * HINT: Make sure to pass in the audioSrc as parameter to create a new audio track
- * HINT: Create a play function to play the audio if called 
+ * HINT: Create a play function to play the audio if called
  */
 class Beat {
+  constructor(audioSrc) {
+    this.audio = new Audio(audioSrc);
+  }
+  play() {
+    this.audio.currentTime = 0;
+    this.audio.play();
+  }
 }
-
-
 
 /**
  * Button class that keeps track of the button color based on a press
  */
 class Button {
-    constructor(color, keyCode){
-    }
+  constructor(color, keyCode) {
+    this.color = color;
+    this.keyCode = keyCode;
+    this.element = document.getElementById(keyCode);
+    this.setButtonColorInHTML();
+    this.createTransitionEndListener();
+  }
 
-    /**
-     * Set the button color based on color specified
-     */
-    setButtonColorInHTML = () => {
-    }
+  /**
+   * Set the button color based on color specified
+   */
+  setButtonColorInHTML = () => {
+    this.element.style.borderColor = this.color;
+  };
 
-    /**
-     * Select function to set the background color and boxShadow
-     */
-    select = () => {
-    }
+  createTransitionEndListener = () => {
+    this.element.addEventListener("transitionend", () => {
+      this.deselect();
+    });
+  };
 
-    /**
-     * Deselect function to reset background color and boxShadow
-     */
-    deselect = () => {
-    }
+  /**
+   * Select function to set the background color and boxShadow
+   */
+  select = () => {
+    this.element.style.backgroundColor = this.color;
+    this.element.style.boxShadow = `0px 0px 17px 0px ${this.color}`;
+  };
+
+  /**
+   * Deselect function to reset background color and boxShadow
+   */
+  deselect = () => {
+    document.getElementById(this.keyCode).style.backgroundColor = "transparent";
+    document.getElementById(this.keyCode).style.boxShadow = "";
+  };
 }
